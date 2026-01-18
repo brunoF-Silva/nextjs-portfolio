33ce9bcd32a0cd896e0b4e2b9deb372d204c784e
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2026-01-18 20:20:02 -0300

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/home/hero/HeroSection";
 
 export default function Home() {
   return (
-    <h1 className="h-[2000px]">Hello everybody</h1>
+    <>
+    <HeroSection/>
+    </>
   );
 }
--- src/components/general/LinkButton.tsx
+++ src/components/general/LinkButton.tsx
@@ -8,7 +8,7 @@ interface LinkButtonProps {
   icon?:IconType,
   iconPosition?: "right" | "left",
   rounded?:boolean,
-  download: boolean,
+  download?: boolean,
 
 }
 
