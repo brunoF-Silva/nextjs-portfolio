4a3b5f694877c7d8a4230875a7d2d677bc4717b3
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2026-01-29 20:21:54 -0300

    feat: add resume section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/home/hero/HeroSection";
 import ServicesSection from "@/components/home/services/ServicesSection";
+import ResumeSection from "@/components/resume/ResumeSection";
 
 export default function Home() {
   return (
     <>
     <HeroSection/>
     <ServicesSection/>
+    <ResumeSection/>
     </>
   );
 }
