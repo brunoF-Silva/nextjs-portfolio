3ee25b55e60f3718c8a368f210fff30065e0ba7f
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2026-02-21 17:01:54 -0300

    chore: update projects section
    ## What it does:
    This commit updates the projects section by changing two images and making them clickable, redirecting users to the projects themselves.

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -4,26 +4,28 @@ interface ProjectCardProps {
   imagePath: string;
   title: string;
   description: string;
+  link: string;
 }
 
 export default function ProjectCard({
   imagePath,
   title,
   description,
+  link,
 }: ProjectCardProps) {
   return (
-    <div>
+    <a href={link} target="_blank" rel="noopener noreferrer">
       <Image
         src={imagePath}
         alt="project-image"
         width={800}
         height={600}
-        className="rounded-lg"
+        className="rounded-lg transition hover:scale-102"
       />
       <p className="my-4 text-xl sm:text-2xl font-semibold text-gray-200">
         {title}
       </p>
       <p className="font-medium text-gray-400">{description}</p>
-    </div>
+    </a>
   );
 }
